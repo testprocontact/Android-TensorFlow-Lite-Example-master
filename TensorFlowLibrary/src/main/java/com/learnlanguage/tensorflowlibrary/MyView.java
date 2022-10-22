8e5fc08b9dd821d5ae9342f3c3658f64299c6134
apphot <apphot.contact@gmail.com>
2022-10-22 21:16:34 +0700

    Initial commit

---
+++ TensorFlowLibrary/src/main/java/com/learnlanguage/tensorflowlibrary/MyView.java
@@ -0,0 +1,17 @@
+package com.learnlanguage.tensorflowlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MyView extends LinearLayout {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
